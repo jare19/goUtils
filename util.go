883cc092a49e851597e178cc2f6cf788da0460a3
Jason Reeves <jason.reeves@target.com>
2018-07-06 16:07:54 -0500

    first commit

---
+++ util.go
@@ -0,0 +1,41 @@
+package goUtils
+
+import (
+	"bufio"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+)
+
+// PrettyPrint prints out human readable json from things that are not
+func PrettyPrint(i interface{}) {
+	x, _ := json.MarshalIndent(i, "", "  ")
+	fmt.Println(string(x))
+}
+
+// AreYouSure Are you sure?
+func AreYouSure(i interface{}) bool {
+	answer := false
+	Scanner := bufio.NewReader(os.Stdin)
+	fmt.Printf("\nThis is about to happen:\n%v\nARE YOU SURE!?!?! (Y/n)", i)
+	result, err := Scanner.ReadString('\n')
+	if err != nil {
+		fmt.Println("Something went horribly wrong, try again")
+		os.Exit(1)
+	}
+	result2 := strings.ToLower(strings.TrimSpace(result))
+	if result2 == "y" || result2 == "yes" {
+		answer = true
+	}
+	return answer
+}
+
+// GetMD5Hash returns a MD5 hash of a string
+func GetMD5Hash(text string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(text))
+	return hex.EncodeToString(hasher.Sum(nil))
+}
